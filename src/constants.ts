8367d345b7aee6afcb67992428fabb95464f049b
Joel Besada <joel.besada@shopify.com>
2020-06-03 18:18:55 +0200

    Add support for both interpolation syntaxes ({{foo}} and %{bar})

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const MODULE_NAME = 'i18n-js';
 export const CONFIG_NAME = `i18n-dts`;
 export const OUTPUT_FILE_NAME = `${MODULE_NAME}.d.ts`;
 export const PACKAGE_JSON = 'package.json';
-export const INTERPOLATION_PATTERN = /\{\{([a-zA-Z0-9]+)\}\}/;
+export const INTERPOLATION_PATTERN = /(?:\{\{|%\{)([a-zA-Z0-9]+)(?:\}\}?)/;
 export const NEWLINE = '\n\n';
